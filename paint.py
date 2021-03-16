348310e57143cb5cdb5198222ab6888739240975
DiegoPython <diegorscorpion@gmail.com>
2021-03-16 12:13:46 -0600

    Circulo
    Se agregó la función para dibujar círculos en el juego

--- paint.py
+++ paint.py
@@ -21,9 +21,14 @@ def square(start, end):
 
     end_fill()
 
-def circle(start, end):
+def circ(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    circle(end.x - start.x)
+    end_fill()
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
@@ -61,7 +66,7 @@ onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', circ), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
